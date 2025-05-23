9e026f7985e4005f1f5ff337ddfdeb7944b4f5f1
Timo Salm <accounts@timosalm.de>
2025-05-23 08:53:32 +0200

    Improvements

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.orderservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -20,6 +22,9 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 public class OrderServiceApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderServiceApplication.class);
+
+
     @LoadBalanced
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
@@ -36,6 +41,15 @@ public class OrderServiceApplication {
 
                     AbstractOAuth2Token token = (AbstractOAuth2Token) authentication.getCredentials();
                     request.getHeaders().setBearerAuth(token.getTokenValue());
+                    return execution.execute(request, body);
+                },
+                (request, body, execution) -> {
+                    log.info("===========================request begin================================================");
+                    log.info("URI         : {}", request.getURI());
+                    log.info("Method      : {}", request.getMethod());
+                    log.info("Headers     : {}", request.getHeaders() );
+                    log.info("Request body: {}", new String(body, "UTF-8"));
+                    log.info("==========================request end================================================");
                     return execution.execute(request, body);
                 }).build();
     }
