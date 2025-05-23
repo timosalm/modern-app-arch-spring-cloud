bf75533f6631ea82eebed4029afd75c92a41b17c
Timo Salm <accounts@timosalm.de>
2025-05-23 02:16:24 +0200

    Improvements

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ./mvnw spring-boot:build-image
 docker compose up
 ```
+
+```
+GATEWAY_URL="http://0.0.0.0:$(docker compose port gateway 8080 | awk -F ':' '{print $2}')"
+curl $GATEWAY_URL/services/product-service/api/v1/products
+curl $GATEWAY_URL/services/order-service/api/v1/orders
+curl -XPOST "$GATEWAY_URL/services/order-service/api/v1/orders" --header "Content-Type: application/json" --data '{"productId":1,"shippingAddress":"Test address"}'
+     
+```
--- gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
+++ gateway/src/main/java/com/example/gateway/WebSecurityConfiguration.java
@@ -1,7 +1,8 @@
-/*package com.example.gateway;
+package com.example.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,12 +17,13 @@ class WebSecurityConfiguration {
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		return http
 				.authorizeExchange(authorize -> authorize
-						.pathMatchers("/services/**").authenticated()
+						//.pathMatchers("/services/**").authenticated()
 						.pathMatchers("/**").permitAll()
-				)
+				).csrf(ServerHttpSecurity.CsrfSpec::disable)
+				/*
 				.oauth2ResourceServer((resourceServer) -> resourceServer
 						.jwt(withDefaults())
-				)
+				)*/
 				.build();
 	}
-}*/
+}
