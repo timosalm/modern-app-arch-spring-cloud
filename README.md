7e210ba412e5c1cc68e780204b818eab1db07e26
Timo Salm <accounts@timosalm.de>
2025-05-23 08:19:27 +0200

    Improvements

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ docker compose up
 ACCESS_TOKEN=
 GATEWAY_URL="http://0.0.0.0:$(docker compose port gateway 8080 | awk -F ':' '{print $2}')"
 curl $GATEWAY_URL/services/product-service/api/v1/products --header "Authorization: Bearer $ACCESS_TOKEN"
-curl $GATEWAY_URL/services/order-service/api/v1/orders
-curl -XPOST "$GATEWAY_URL/services/order-service/api/v1/orders" --header "Content-Type: application/json" --data '{"productId":1,"shippingAddress":"Test address"}'
+curl $GATEWAY_URL/services/order-service/api/v1/orders --header "Authorization: Bearer $ACCESS_TOKEN"
+curl -XPOST "$GATEWAY_URL/services/order-service/api/v1/orders" --header "Content-Type: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" --data '{"productId":1,"shippingAddress":"Test address"}'
      
 ```
--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.orderservice;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cache.annotation.EnableCaching;
 
@@ -17,12 +23,33 @@ public class OrderServiceApplication {
     @LoadBalanced
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+        return restTemplateBuilder.additionalInterceptors(
+                (request, body, execution) -> {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    if (authentication == null) {
+                        return execution.execute(request, body);
+                    }
+
+                    if (!(authentication.getCredentials() instanceof AbstractOAuth2Token)) {
+                        return execution.execute(request, body);
+                    }
+
+                    AbstractOAuth2Token token = (AbstractOAuth2Token) authentication.getCredentials();
+                    request.getHeaders().setBearerAuth(token.getTokenValue());
+                    return execution.execute(request, body);
+                }).build();
     }
 
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setObservationEnabled(true);
+        return rabbitTemplate;
     }
 
     public static void main(String[] args) {
